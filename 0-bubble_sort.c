bc847b6ea934127b0f73f6d9456dfb6734f638d0
setalxswe <setalxswe@gmail.com>
2023-10-22 05:24:52 -0700

    --UpDate--

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,29 +6,39 @@
  * @size: size of the array
  */
 
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+	int swapped;
+
+	if (array == NULL || size < 2)
+		return;
+
+	/* no of passes required to sort the array*/
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		/* check if any swaps occurred in this pass*/
+
+		/*Compare and swap adjacent elements*/
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				/*Swap the elements*/
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1; /*Set swapped flag*/
+				print_array(array, size);
+			}
+		}
+
+		/*If no swaps occurred, the array is already sorted*/
+		if (swapped == 0)
+		{
+			return;
+		}
+	}
 }
-
-void bubble_sort(int *array, size_t size) {
-    if (array == NULL || size < 2) {
-        return;
-    }
-
-    for (size_t i = 0; i < size - 1; i++) {
-        int swapped = 0;
-        for (size_t j = 0; j < size - i - 1; j++) {
-            if (array[j] > array[j + 1]) {
-                swap(&array[j], &array[j + 1]);
-                swapped = 1;
-                print_array(array, size);
-            }
-        }
-        if (swapped == 0) {
-            return;
-        }
-    }
-}
-
