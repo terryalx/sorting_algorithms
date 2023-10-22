0e6e8f93f55c98d73656df4b00bd85806df5e505
setalxswe <setalxswe@gmail.com>
2023-10-22 06:55:18 -0700

    __update__

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,7 +9,7 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t a, j;
 	int temp;
 	int swapped;
 
@@ -17,10 +17,10 @@ void bubble_sort(int *array, size_t size)
 	{
 		return;
 	}
-	for (i = 0; i < size - 1; i++)
+	for (a = 0; a < size - 1; a++)
 	{
 		swapped = 0;
-		for (j = 0; j < size - i - 1; j++)
+		for (j = 0; j < size - j - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
