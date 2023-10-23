564396703dfa40d0eeb6ab337ed78a8d9f65e3f5
setalxswe <setalxswe@gmail.com>
2023-10-23 12:15:45 -0700

    __1-insertion_sort_list.c__

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,6 +1,26 @@
 #include "sort.h"
 
 /**
+ * swap - swaps 2 nodes in a doubly-linked list
+ * @a: address of first node
+ * @b: address of second node
+ *
+ * Return: void
+ */
+void swap(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+
+}
+
+/**
  * insertion_sort_list - insertion sorts a doubly-linked list
  * @list: address of pointer to head node
  *
@@ -11,10 +31,7 @@ void insertion_sort_list(listint_t **list)
 	listint_t *i, *j;
 
 	if (!list || !*list || !(*list)->next)
-	{
 		return;
-	}
-
 	i = (*list)->next;
 	while (i)
 	{
