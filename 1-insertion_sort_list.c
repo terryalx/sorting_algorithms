c94b0b69cbb7df7676d6d37308479dae7d2263f3
setalxswe <setalxswe@gmail.com>
2023-10-23 12:43:57 -0700

    --updateswap--

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,69 +1,36 @@
 #include "sort.h"
 
 /**
- * swap - swap two nodes in a doubly-linked list
- * @a: first node
- * @b: second node
+ * bubble_sort - Sorts an array of integers in ascending
+ * order using the bubble sort algorithm.
  *
- * Return: void
+ * @array: The array to be sorted.
+ * @size: Size of the array.
  */
-void swap(listint_t *a, listint_t *b)
-{
-	if (a->prev)
-	{
-		a->prev->next = b;
-	}
-	if (b->next)
-	{
-		b->next->prev = a;
-	}
-	a->next = b->next;
-	b->prev = a->prev;
-	a->prev = b;
-	b->next = a;
+void bubble_sort(int *array, size_t size) {
+	size_t i, j;
+	int temp;
+	int swapped;
 
-}
-
-/**
- * insertion_sort_list - perform an insertion sort on a
- * doubly-linked list
- * @list: pointer to head node
- *
- * Return: void
- */
-void insertion_sort_list(listint_t **list)
-{
-	listint_t *i, *j;
-
-	if (head == NULL)
-	{
+	if (array == NULL || size < 2) {
 		return;
 	}
-	if (*head == NULL)
-	{
-		return;
-	}
-	if ((*head)->next == NULL)
-	{
-		return;
-	}
-	i = (*list)->next;
-	while (i)
-	{
-		j = i;
-		i = i->next;
-		while (j && j->prev)
-		{
-			if (j->prev->n > j->n)
-			{
-				swap(j->prev, j);
-				if (!j->prev)
-					*list = j;
-				print_list((const listint_t *)*list);
+
+	for (i = 0; i < size - 1; i++) {
+		swapped = 0;
+
+		for (j = 0; j < size - i - 1; j++) {
+			if (array[j] > array[j + 1]) {
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				swapped = 1;
+				print_array(array, size);
 			}
-			else
-				j = j->prev;
 		}
-
+		if (swapped == 0) {
+			return;
+		}
 	}
 }
+
