6db826ca29663f7cd09d9eb2830740dad282634e
setalxswe <setalxswe@gmail.com>
2023-10-23 12:17:56 -0700

    __1-insertion_sort_list.c__

--- swap_int.c
+++ swap_int.c
@@ -1,22 +1,18 @@
 #include "sort.h"
 
 /**
- * swap - doubly-linked list swaps two elements
- * @a: first node
- * @b: second node
+ * swap - swaps 2 nodes in a doubly-linked list
+ * @a: address of first node
+ * @b: address of second node
  *
  * Return: void
  */
 void swap(listint_t *a, listint_t *b)
 {
 	if (a->prev)
-	{
 		a->prev->next = b;
-	}
 	if (b->next)
-	{
 		b->next->prev = a;
-	}
 	a->next = b->next;
 	b->prev = a->prev;
 	a->prev = b;
