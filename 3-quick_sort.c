edec67da7c89a1abfd85822cfbe9acda084ccfae
setalxswe <setalxswe@gmail.com>
2023-10-24 13:58:25 -0700

    3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -7,13 +7,10 @@
  *
  * Return: NULL
  */
-void swap_int(int *a, int *b)
-{
-	int temp;
-
-	*a = *b;
-	*b = temp;
-	temp = *a;
+void swap_int(int *a, int *b) {
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
 }
 
 /**
