5b6b132133559ea0861c3ba7aae7be920c7de952
setalxswe <setalxswe@gmail.com>
2023-10-24 11:15:56 -0700

    2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,18 +16,18 @@ void selection_sort(int *array, size_t size)
 	{
 		jMin = i;
 		for (j = i + 1; j < size; j++)
-		{
 			if (array[j] < array[jMin])
-				jMin = j;
-
-			if (jMin != i)
 			{
-				tmp = array[i];
-				array[i] = array[jMin];
-				array[jMin] = tmp;
-
-				print_array(array, size);
+				jMin = j;
 			}
+
+		if (jMin != i)
+		{
+			tmp = array[i];
+			array[i] = array[jMin];
+			array[jMin] = tmp;
+
+			print_array(array, size);
 		}
 	}
 }
